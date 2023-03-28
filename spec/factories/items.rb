100fbbe0d50d3423118d5d5df4afd2e03a30629b
Conner Van Loan <cdvanloan@gmail.com>
2023-03-28 12:13:50 -0600

    sets up factories and first merchant tests

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { Faker::Name.first_name}
     description { Faker::ChuckNorris.fact }
     unit_price {Faker::Number.decimal(l_digits: 2)}
-    association :merchant
+    # merchant_id {Faker::Number.number(digits: 1..2)}
   end
 end
